962bce70e410dbb0918176b13c65f60424c0e1b8
NGUYEN_ANH_QUAN <nguyenanhquangroup@gmail.com>
2023-07-23 18:38:27 +0700

    done ex2

--- exercises/ownership-borrowing/src/lib.rs
+++ exercises/ownership-borrowing/src/lib.rs
@@ -3,7 +3,7 @@
 fn exercise1() {
     // Use as many approaches as you can to make it work
     let x = String::from("hello, world");
-    let y = x;
+    let y = x.clone();
     let z = x;
 }
 
@@ -17,8 +17,9 @@ fn exercise2() {
     println!("{}", s2);
 }
 // Only modify the code below!
-fn take_ownership(s: String) {
+fn take_ownership(s: String) -> String {
     println!("{}", s);
+    s
 }
 
 // Exercise 3
@@ -33,78 +34,84 @@ fn exercise3() {
 
     let values_number = values.len();
 
-    let additions: Vec<usize> = vec![0];
+    let mut additions: Vec<usize> = vec![0]; // Fix 1: Correct the type of "additions"
 
     println!("{:?}", values_number);
 
-    while additions.len() > 0 {
+    let max_iterations = 10; // Set a maximum number of iterations for the loop to avoid infinite loop
+
+    let mut iterations = 0; // Initialize the iteration counter
+
+    while iterations < max_iterations && additions.len() > 0 { // Fix 2: Add a condition to stop the loop
         let mut addition: f64 = 0.0;
 
         // Sumar valores en additions
-        for element_index in additions {
+        for &element_index in &additions { // Fix 3: Use "&" to borrow the element_index
             let addition_aux = values[element_index];
             addition = addition_aux + addition;
         }
+
+        iterations += 1; // Increment the iteration counter
     }
 }
+
 
 // Exercise 4
 // Make it compile
 fn exercise4(value: u32) -> &'static str {
     let str_value = value.to_string(); // Convert u32 to String
-    let str_ref: &str = &str_value; // Obtain a reference to the String
+    static  str_ref: &str = &str_value; // Obtain a reference to the String
     str_ref // Return the reference to the String
 }
 
-// Exercise 5
-// Make it compile
 use std::collections::HashMap;
+
 fn exercise5() {
     let mut my_map = HashMap::from([(1, "1.0".to_string()), (2, "2.0".to_string())]);
 
     let key = 3;
 
     let res = match my_map.get(&key) {
-        Some(child) => child,
+        Some(child) => child.as_str(),
         None => {
-            let value = "3.0".to_string();
-            my_map.insert(key, value);
-            &value // HERE IT FAILS
+            let value = "3.0";
+            my_map.insert(key, value.to_string());
+            value // Return a static string slice directly
         }
     };
 
     println!("{}", res);
 }
 
-// Exercise 6
-// Make it compile
 
 use std::io;
 
 fn exercise6() {
-    let mut prev_key: &str = "";
+    let mut prev_key = String::new(); // Fix 1: Change the type of prev_key to String
 
-    for line in io::stdin().lines() {
+    for line in io::stdin().lock().lines() { // Fix 2: Use io::stdin().lock() to prevent conflicts
         let s = line.unwrap();
 
-        let data: Vec<&str> = s.split("\t").collect();
-        if prev_key.len() == 0 {
-            prev_key = data[0];
+        let data: Vec<&str> = s.split('\t').collect(); // Fix 3: Use single quotes for the tab character
+
+        if prev_key.is_empty() { // Fix 4: Use is_empty() to check if prev_key is empty
+            prev_key = data[0].to_string(); // Fix 5: Convert &str to String
         }
     }
 }
 
-// Exercise 7
-// Make it compile
+
 fn exercise7() {
     let mut v: Vec<&str> = Vec::new();
+    let s: &str;
     {
         let chars = [b'x', b'y', b'z'];
-        let s: &str = std::str::from_utf8(&chars).unwrap();
-        v.push(&s);
+        s = std::str::from_utf8(&chars).unwrap();
+        v.push(s);
     }
     println!("{:?}", v);
 }
+
 
 // Exercise 8
 // Make it compile
